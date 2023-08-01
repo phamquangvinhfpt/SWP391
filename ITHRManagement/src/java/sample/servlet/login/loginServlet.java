603a7d3518d17a5e1b1e7a0ce478af25a3f272d6
Phạm Quang Vinh <Vinhpqse160633@fpt.edu.vn>
2023-08-01 12:47:17 +0700

    change

--- ITHRManagement/src/java/sample/servlet/login/loginServlet.java
+++ ITHRManagement/src/java/sample/servlet/login/loginServlet.java
@@ -3,20 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sample.servlet;
+package sample.servlet.login;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import sample.dao.UserDAO;
 import sample.dto.User;
 
@@ -24,6 +23,7 @@ import sample.dto.User;
  *
  * @author Admin
  */
+@WebServlet(name = "loginServlet", urlPatterns = {"/loginServlet"})
 public class loginServlet extends HttpServlet {
 
     /**
