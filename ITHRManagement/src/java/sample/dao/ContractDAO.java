3f7cb3e8145f8b178b535221dc44fbf790c0a61d
ToanDC28 <toandcse160630@fpt.edu.vn>
2023-08-03 12:18:35 +0700

    contract

--- ITHRManagement/src/java/sample/dao/ContractDAO.java
+++ ITHRManagement/src/java/sample/dao/ContractDAO.java
@@ -63,4 +63,42 @@ public class ContractDAO {
             
         }
     }
+    public void getContractByStaffID(String User_id){
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ContractDTO dto = null;
+        try{
+            con = DBUtils.makeConnection();
+            if(con != null){
+                String sql = "Select ID, User_ID, StartDate, EndDate, Job, Basic_Salary, "
+                        + "(select Name from Salary_Type where ID = c.SalaryType_ID) as type, Status from Contract as c"
+                        + " Where User_ID = ?";
+                
+                ps = con.prepareStatement(sql);
+                ps.setString(1, User_id);
+                rs = ps.executeQuery();
+                while(rs.next()){
+                    int id = rs.getInt("ID");
+                    Date sDate = rs.getDate("StartDate");
+                    Date eDate = rs.getDate("EndDate");
+                    String job = rs.getString("Job");
+                    double bSal = rs.getDouble("Basic_Salary");
+                    String type = rs.getString("type");
+                    boolean status = rs.getBoolean("Status");
+                    dto = new ContractDTO(id, User_id, sDate, eDate, job, bSal, type, status);
+                    if(this.listContract == null){
+                        listContract = new ArrayList<>();
+                        
+                    }
+                    this.listContract.add(dto);
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ContractDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            
+        }
+    }
+    
 }
